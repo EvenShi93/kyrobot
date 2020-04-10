601e2f6c7deb082b3cccee43f83ea30595803783
kyChu <hello.kychu@gmail.com>
2020-04-10 11:54:06 +0800

    update

--- ctrl/sysconfig/SysConfig.h
+++ ctrl/sysconfig/SysConfig.h
@@ -11,9 +11,7 @@
 
 #include "log.h"
 
-#include "boardconfig.h"
-
-#define FREERTOS_ENABLED                         (1)
+#include "config.h"
 
 #if FREERTOS_ENABLED
 #define kmm_alloc                                pvPortMalloc
@@ -25,49 +23,17 @@
 #define kmm_free                                 free
 #endif /* FREERTOS_ENABLED */
 
-/* Interrupt priority list */
-#define INT_PRIORITY_MAX                         configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY
-#define INT_PRIORITY_MIN                         ((1 << __NVIC_PRIO_BITS) - 1)
+#if FREERTOS_ENABLED
+#define delay                               osDelay
+#else
+#define delay                               HAL_Delay
+#endif /* FREERTOS_ENABLED */
 
-#define INT_PRIORITY_LIMIT_MAX(prio)             ((prio) < INT_PRIORITY_MAX) ? INT_PRIORITY_MAX : (prio)
-#define INT_PRIORITY_LIMIT_MIN(prio)             ((prio) > INT_PRIORITY_MIN) ? INT_PRIORITY_MIN : (prio)
+#define UNUSED_VARIABLE(X)                       ((void)(X))
+#define UNUSED_PARAMETER(X)                      UNUSED_VARIABLE(X)
+#define UNUSED_RETURN_VALUE(X)                   UNUSED_VARIABLE(X)
 
-#define INT_PRIORITY_LIMIT(prio)                 INT_PRIORITY_LIMIT_MIN(INT_PRIORITY_LIMIT_MAX(prio))
-
-#define INT_PRIO_IMUIF_DMARX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 0)
-#define INT_PRIO_IMUIF_DMATX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 0)
-
-#define INT_PRIO_IMUIF_INT1                      INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 1)
-#define INT_PRIO_IMUIF_INT2                      INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 9)
-
-#define INT_PRIO_GSMIF_PERIPH                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 4)
-#define INT_PRIO_GSMIF_DMARX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 4)
-#define INT_PRIO_GSMIF_DMATX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 4)
-
-#define INT_PRIO_UBXAIF_PERIPH                   INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 6)
-#define INT_PRIO_UBXAIF_DMARX                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 6)
-#define INT_PRIO_UBXAIF_DMATX                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 6)
-
-#define INT_PRIO_UBXBIF_PERIPH                   INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 7)
-#define INT_PRIO_UBXBIF_DMARX                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 7)
-#define INT_PRIO_UBXBIF_DMATX                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 7)
-
-#define INT_PRIO_MAGIF_DMARX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 8)
-#define INT_PRIO_MAGIF_DMATX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 8)
-#define INT_PRIO_MAGIF_ER                        INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 9)
-#define INT_PRIO_MAGIF_EV                        INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 9)
-
-#define INT_PRIO_COMIF_PERIPH                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 10)
-#define INT_PRIO_COMIF_DMARX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 10)
-#define INT_PRIO_COMIF_DMATX                     INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 10)
-
-#define INT_PRIO_OTGFS_INT                       INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 9)
-
-#define INT_PRIO_FLASHIF_IT                      INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 2)
-
-#define INT_PRIO_RTCMIF_PERIPH                   INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 7)
-#define INT_PRIO_RTCMIF_DMARX                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 5)
-#define INT_PRIO_RTCMIF_DMATX                    INT_PRIORITY_LIMIT(INT_PRIORITY_MAX + 7)
+void APP_StartThread(void const *argument);
 
 #endif /* __SYSCONFIG_H */
 
