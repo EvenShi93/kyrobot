90a982e807e6f9f9dfcd176006fa11850afd431e
kyChu <hello.kychu@gmail.com>
2020-04-14 17:15:30 +0800

    format code

--- ctrl/middleware/log/log_buffers.c
+++ ctrl/middleware/log/log_buffers.c
@@ -17,7 +17,6 @@
 #if CONFIG_LOG_ENABLE
 void log_buffer_hex(const char *tag, const void *buffer, uint16_t buff_len, log_level_t log_level)
 {
-//#if CONFIG_LOG_ENABLE
   if (buff_len == 0) {
     return;
   }
@@ -32,13 +31,13 @@ void log_buffer_hex(const char *tag, const void *buffer, uint16_t buff_len, log_
     } else {
       bytes_cur_line = buff_len;
     }
-//    if (!esp_ptr_byte_accessible(buffer)) {
+//    if (!ptr_byte_accessible(buffer)) {
       //use memcpy to get around alignment issue
 //      memcpy(temp_buffer, buffer, (bytes_cur_line + 3) / 4 * 4);
 //      ptr_line = temp_buffer;
 //    } else {
       ptr_line = buffer;
-//    }s
+//    }
 
     for (int i = 0; i < bytes_cur_line; i ++) {
       sprintf(hex_buffer + 3 * i, "%02x ", ptr_line[i]);
@@ -47,17 +46,10 @@ void log_buffer_hex(const char *tag, const void *buffer, uint16_t buff_len, log_
     buffer += bytes_cur_line;
     buff_len -= bytes_cur_line;
   } while (buff_len);
-//#else
-//  (void)tag;
-//  (void)buffer;
-//  (void)buff_len;
-//  (void)log_level;
-//#endif /* CONFIG_LOG_ENABLE */
 }
 
 void log_buffer_char(const char *tag, const void *buffer, uint16_t buff_len, log_level_t log_level)
 {
-//#if CONFIG_LOG_ENABLE
   if (buff_len == 0) {
     return;
   }
@@ -72,7 +64,7 @@ void log_buffer_char(const char *tag, const void *buffer, uint16_t buff_len, log
     } else {
       bytes_cur_line = buff_len;
     }
-//    if (!esp_ptr_byte_accessible(buffer)) {
+//    if (!ptr_byte_accessible(buffer)) {
       //use memcpy to get around alignment issue
 //      memcpy(temp_buffer, buffer, (bytes_cur_line + 3) / 4 * 4);
 //      ptr_line = temp_buffer;
@@ -83,21 +75,14 @@ void log_buffer_char(const char *tag, const void *buffer, uint16_t buff_len, log
     for (int i = 0; i < bytes_cur_line; i ++) {
       sprintf(char_buffer + i, "%c", ptr_line[i]);
     }
-        LOG_LEVEL(log_level, tag, "%s", char_buffer);
-        buffer += bytes_cur_line;
-        buff_len -= bytes_cur_line;
-    } while (buff_len);
-//#else
-//  (void)tag;
-//  (void)buffer;
-//  (void)buff_len;
-//  (void)log_level;
-//#endif /* CONFIG_LOG_ENABLE */
+    LOG_LEVEL(log_level, tag, "%s", char_buffer);
+    buffer += bytes_cur_line;
+    buff_len -= bytes_cur_line;
+  } while (buff_len);
 }
 
 void log_buffer_hexdump(const char *tag, const void *buffer, uint16_t buff_len, log_level_t log_level)
 {
-//#if CONFIG_LOG_ENABLE
   if (buff_len == 0) {
     return;
   }
@@ -115,46 +100,40 @@ void log_buffer_hexdump(const char *tag, const void *buffer, uint16_t buff_len, 
     } else {
       bytes_cur_line = buff_len;
     }
-//        if (!esp_ptr_byte_accessible(buffer)) {
-            //use memcpy to get around alignment issue
-//            memcpy(temp_buffer, buffer, (bytes_cur_line + 3) / 4 * 4);
-//            ptr_line = temp_buffer;
-//        } else {
-            ptr_line = buffer;
-//        }
-        ptr_hd = hd_buffer;
+//    if (!ptr_byte_accessible(buffer)) {
+      //use memcpy to get around alignment issue
+//      memcpy(temp_buffer, buffer, (bytes_cur_line + 3) / 4 * 4);
+//      ptr_line = temp_buffer;
+//    } else {
+      ptr_line = buffer;
+//    }
+    ptr_hd = hd_buffer;
 
-        ptr_hd += sprintf(ptr_hd, "%p ", buffer);
-        for (int i = 0; i < BYTES_PER_LINE; i ++) {
-            if ((i & 7) == 0) {
-                ptr_hd += sprintf(ptr_hd, " ");
-            }
-            if (i < bytes_cur_line) {
-                ptr_hd += sprintf(ptr_hd, " %02x", ptr_line[i]);
-            } else {
-                ptr_hd += sprintf(ptr_hd, "   ");
-            }
-        }
-        ptr_hd += sprintf(ptr_hd, "  |");
-        for (int i = 0; i < bytes_cur_line; i ++) {
-            if (isprint((int)ptr_line[i])) {
-                ptr_hd += sprintf(ptr_hd, "%c", ptr_line[i]);
-            } else {
-                ptr_hd += sprintf(ptr_hd, ".");
-            }
-        }
-        ptr_hd += sprintf(ptr_hd, "|");
+    ptr_hd += sprintf(ptr_hd, "%p ", buffer);
+    for (int i = 0; i < BYTES_PER_LINE; i ++) {
+      if ((i & 7) == 0) {
+        ptr_hd += sprintf(ptr_hd, " ");
+      }
+      if (i < bytes_cur_line) {
+        ptr_hd += sprintf(ptr_hd, " %02x", ptr_line[i]);
+      } else {
+        ptr_hd += sprintf(ptr_hd, "   ");
+      }
+    }
+    ptr_hd += sprintf(ptr_hd, "  |");
+    for (int i = 0; i < bytes_cur_line; i ++) {
+      if (isprint((int)ptr_line[i])) {
+        ptr_hd += sprintf(ptr_hd, "%c", ptr_line[i]);
+      } else {
+        ptr_hd += sprintf(ptr_hd, ".");
+      }
+    }
+    ptr_hd += sprintf(ptr_hd, "|");
 
-        LOG_LEVEL(log_level, tag, "%s", hd_buffer);
-        buffer += bytes_cur_line;
-        buff_len -= bytes_cur_line;
-    } while (buff_len);
-//#else
-//  (void)tag;
-//  (void)buffer;
-//  (void)buff_len;
-//  (void)log_level;
-//#endif /* CONFIG_LOG_ENABLE */
+    LOG_LEVEL(log_level, tag, "%s", hd_buffer);
+    buffer += bytes_cur_line;
+    buff_len -= bytes_cur_line;
+  } while (buff_len);
 }
 
 #else
