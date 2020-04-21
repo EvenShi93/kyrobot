a633a525b0e98b0c200903ab22ab9da24fbe5741
kyChu <hello.kychu@gmail.com>
2020-04-21 22:54:36 +0800

    add gui demo

---
+++ ctrl/apps/gui/gui_demo.h
@@ -0,0 +1,17 @@
+/*
+ * gui_demo.h
+ *
+ *  Created on: Apr 21, 2020
+ *      Author: kychu
+ */
+
+#ifndef GUI_GUI_DEMO_H_
+#define GUI_GUI_DEMO_H_
+
+#include "GUI.h"
+
+#include "WM.h"
+
+void gui_graph_start(void);
+
+#endif /* GUI_GUI_DEMO_H_ */
