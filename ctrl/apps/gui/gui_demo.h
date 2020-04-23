003f7e10aceae818843715cc1fdd8e9f6ab5ccbb
kyChu <hello.kychu@gmail.com>
2020-04-23 10:32:26 +0800

    include new gui module

--- ctrl/apps/gui/gui_demo.h
+++ ctrl/apps/gui/gui_demo.h
@@ -14,6 +14,10 @@
 
 void gui_graph_start(void);
 int gui_iconview_start(void);
-void gui_setting_menu_start(void);
+int gui_setting_menu_start(void);
+void gui_config_backlight_start(void);
+
+void gui_ctrl_motor_start(void);
+void gui_ctrl_steer_start(void);
 
 #endif /* GUI_GUI_DEMO_H_ */
