6f969b9f03c3fbe01a5ec72a693ac1b5e9f4998b
kyChu <hello.kychu@gmail.com>
2020-04-23 10:26:29 +0800

    add steer driver module

--- ctrl/driver/bsp/subdir.mk
+++ ctrl/driver/bsp/subdir.mk
@@ -22,6 +22,7 @@ $(BuildPath)/driver/bsp/imuif.o \
 $(BuildPath)/driver/bsp/magif.o \
 $(BuildPath)/driver/bsp/disp.o \
 $(BuildPath)/driver/bsp/dispif.o \
+$(BuildPath)/driver/bsp/steerif.o \
 $(BuildPath)/driver/bsp/flashif.o \
 $(BuildPath)/driver/bsp/usbdif.o \
 $(BuildPath)/driver/bsp/ist83xx.o \
