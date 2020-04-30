e248258fea35fa085af78c3395443f5d6f2ddefe
kyChu <hello.kychu@gmail.com>
2020-04-30 14:47:06 +0800

    add functions

--- ctrl/middleware/algorithm/commonly.c
+++ ctrl/middleware/algorithm/commonly.c
@@ -30,3 +30,37 @@ float ScalarProduct(Vector3D *va, Vector3D *vb)
 {
   return (va->X * vb->X + va->Y * vb->Y + va->Z * vb->Z);
 }
+
+/*
+ * apply deadband function.
+ */
+float apply_deadband(float value, float deadband)
+{
+	if(fabs(value) <= deadband) {value = 0;}
+
+	if(value > deadband) {value -= deadband;}
+	if(value < -deadband) {value += deadband;}
+
+	return value;
+}
+
+/*
+ * step change function.
+ */
+void step_change(float *in, float target, float step, float deadBand)
+{
+	if(fabsf(*in - target) <= deadBand) {*in = target; return;}
+
+	if(*in > target) {*in -= fabsf(step); return;}
+	if(*in < target) {*in += fabsf(step); return;}
+}
+
+/*
+ * limitation
+ */
+float apply_limit(float in, float min, float max)
+{
+  if(in < min) return min;
+  if(in > max) return max;
+  return in;
+}
