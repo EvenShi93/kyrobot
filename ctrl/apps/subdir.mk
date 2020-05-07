1dd5b4b1d0c2bf76da093ce0f360fdd2f4d72198
kyChu <hello.kychu@gmail.com>
2020-05-07 18:11:09 +0800

    add sd diskio module

--- ctrl/apps/subdir.mk
+++ ctrl/apps/subdir.mk
@@ -5,54 +5,6 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-C_SRCS += \
-./apps/src/apps.c \
-./apps/src/compass.c \
-./apps/src/test_case.c \
-./apps/src/att_est_q.c \
-./apps/src/cpu_utils.c \
-./apps/src/filesystem.c \
-./apps/src/filetransfer.c \
-./apps/src/mesg.c
-
-C_SRCS += \
-./apps/guicfg/gui_drv.c \
-./apps/guicfg/GUIConf.c \
-./apps/guicfg/LCDConf.c \
-./apps/guicfg/gui_x_rtos.c
-
-C_SRCS += \
-./apps/ctrl/ctrl_task.c
-
-ifeq ($(CONFIG_STEMWIN_DEMO_ENABLE), y)
-C_SRCS += \
-./apps/demo/GUIDEMO_Automotive.c \
-./apps/demo/GUIDEMO_Bitmap.c \
-./apps/demo/GUIDEMO.c \
-./apps/demo/GUIDEMO_ColorBar.c \
-./apps/demo/GUIDEMO_Conf.c \
-./apps/demo/GUIDEMO_Cursor.c \
-./apps/demo/GUIDEMO_Graph.c \
-./apps/demo/GUIDEMO_IconView.c \
-./apps/demo/GUIDEMO_Intro.c \
-./apps/demo/GUIDEMO_Listview.c \
-./apps/demo/GUIDEMO_Resource.c \
-./apps/demo/GUIDEMO_Speed.c \
-./apps/demo/GUIDEMO_Start.c \
-./apps/demo/GUIDEMO_TransparentDialog.c \
-./apps/demo/GUIDEMO_Treeview.c
-else
-C_SRCS += \
-./apps/gui/gui_task.c \
-./apps/gui/gui_graph.c \
-./apps/gui/gui_iconview.c \
-./apps/gui/gui_config_bl.c \
-./apps/gui/gui_ctrl_motor.c \
-./apps/gui/gui_ctrl_steer.c \
-./apps/gui/gui_setting_meu.c
-endif
-
-C_SRCS += $(wildcard ./apps/evt/*.c)
 
 OBJS += \
 $(BuildPath)/apps/src/apps.o \
@@ -62,7 +14,8 @@ $(BuildPath)/apps/src/att_est_q.o \
 $(BuildPath)/apps/src/cpu_utils.o \
 $(BuildPath)/apps/src/filesystem.o \
 $(BuildPath)/apps/src/filetransfer.o \
-$(BuildPath)/apps/src/mesg.o
+$(BuildPath)/apps/src/mesg.o \
+$(BuildPath)/apps/src/sd_diskio.o
 
 OBJS += \
 $(BuildPath)/apps/guicfg/gui_drv.o \
