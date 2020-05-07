71068e935b8da6d75c1d64cd15e8d726ffcf3e3a
kyChu <hello.kychu@gmail.com>
2020-05-07 18:02:53 +0800

    initial commit for fatfs diskio driver

---
+++ ctrl/apps/inc/sd_diskio.h
@@ -0,0 +1,13 @@
+/*
+ * sd_diskio.h
+ *
+ *  Created on: May 7, 2020
+ *      Author: kychu
+ */
+
+#ifndef INC_SD_DISKIO_H_
+#define INC_SD_DISKIO_H_
+
+#include "sdcard.h"
+
+#endif /* INC_SD_DISKIO_H_ */
