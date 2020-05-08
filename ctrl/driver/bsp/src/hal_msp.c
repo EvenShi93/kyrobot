31ac2f54900d6bd5c204c131085d95264ede0d02
kyChu <hello.kychu@gmail.com>
2020-05-09 00:28:41 +0800

    UART driver use register callback now

--- ctrl/driver/bsp/src/hal_msp.c
+++ ctrl/driver/bsp/src/hal_msp.c
@@ -17,54 +17,6 @@
 /* Private functions ---------------------------------------------------------*/
 
 /**
-  * @brief UART MSP Initialization 
-  *        This function configures the hardware resources used in this example: 
-  *           - Peripheral's clock enable
-  *           - Peripheral's GPIO Configuration  
-  *           - DMA configuration for transmission request by peripheral 
-  *           - NVIC configuration for DMA interrupt request enable
-  * @param huart: UART handle pointer
-  * @retval None
-  */
-void HAL_UART_MspInit(UART_HandleTypeDef *huart)
-{
-  if(huart->Instance == UBXA_UART) {
-    ubxaif_msp_init(huart);
-  } else if(huart->Instance == COM_UART) {
-    comif_msp_init(huart);
-  } else if(huart->Instance == RMT_UART) {
-    rmtif_msp_init(huart);
-  } else if(huart->Instance == CTRL_UART) {
-    ctrlif_msp_init(huart);
-//  } else if(huart->Instance == UBXB_UART) {
-//    ubxbif_msp_init(huart);
-  }
-}
-
-/**
-  * @brief UART MSP De-Initialization
-  *        This function frees the hardware resources used in this example:
-  *          - Disable the Peripheral's clock
-  *          - Revert GPIO, DMA and NVIC configuration to their default state
-  * @param huart: UART handle pointer
-  * @retval None
-  */
-void HAL_UART_MspDeInit(UART_HandleTypeDef *huart)
-{
-  if(huart->Instance == UBXA_UART) {
-    ubxaif_msp_deinit(huart);
-  } else if(huart->Instance == COM_UART) {
-    comif_msp_deinit(huart);
-  } else if(huart->Instance == RMT_UART) {
-    rmtif_msp_deinit(huart);
-  } else if(huart->Instance == CTRL_UART) {
-    ctrlif_msp_deinit(huart);
-//  } else if(huart->Instance == UBXB_UART) {
-//    ubxbif_msp_deinit(huart);
-  }
-}
-
-/**
   * @brief SPI MSP Initialization
   *        This function configures the hardware resources used in this example:
   *           - Peripheral's clock enable
