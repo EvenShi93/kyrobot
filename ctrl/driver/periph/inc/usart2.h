5f521e8354e5b82a0a83d63e4a6234b61bd5a3b7
kyChu <hello.kychu@gmail.com>
2020-05-08 22:55:51 +0800

    update code structure && add usart2 driver

---
+++ ctrl/driver/periph/inc/usart2.h
@@ -0,0 +1,16 @@
+#ifndef __USART2_H
+#define __USART2_H
+
+#include "SysConfig.h"
+#include "periph_config.h"
+
+status_t usart2_init(uint32_t baudrate);
+status_t usart2_deinit(void);
+status_t usart2_tx_bytes(uint8_t *p, uint32_t l);
+status_t usart2_tx_bytes_it(uint8_t *p, uint32_t l);
+status_t usart2_tx_bytes_dma(uint8_t *p, uint32_t l);
+
+uint32_t usart2_rx_bytes(uint8_t *p, uint32_t l);
+uint32_t usart2_cache_usage(void);
+
+#endif /* __USART2_H */
