61bfa8c8141d323420acb32f6bbbbe3e815fcaa7
kyChu <hello.kychu@gmail.com>
2020-05-09 17:37:10 +0800

    remove unused module

--- ctrl/driver/bsp/subdir.mk
+++ ctrl/driver/bsp/subdir.mk
@@ -6,13 +6,10 @@
 
 OBJS += \
 $(BuildPath)/driver/bsp/callback.o \
-$(BuildPath)/driver/bsp/controlif.o \
 $(BuildPath)/driver/bsp/hal_msp.o \
 $(BuildPath)/driver/bsp/mcu_irq.o \
 $(BuildPath)/driver/bsp/gpio_irq.o \
-$(BuildPath)/driver/bsp/rtcmif.o \
 $(BuildPath)/driver/bsp/timebase.o \
-$(BuildPath)/driver/bsp/ubloxa.o \
 $(BuildPath)/driver/bsp/leds.o \
 $(BuildPath)/driver/bsp/gpios.o \
 $(BuildPath)/driver/bsp/buttons.o \
