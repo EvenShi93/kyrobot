d617e2653998a7a1e46336d5254543c07d98ee43
kyChu <hello.kychu@gmail.com>
2020-05-13 18:22:58 +0800

    uodate in feature

--- ctrl/driver/periph/inc/periph.h
+++ ctrl/driver/periph/inc/periph.h
@@ -13,7 +13,7 @@
 #include "periph_config.h"
 
 typedef struct {
-  const char *uart_name;
+//  const char *uart_name;
   status_t (* uart_init)(uint32_t baudrate);
   status_t (* uart_deinit)(void);
   uint32_t (* uart_rx)(uint8_t *p, uint32_t l);
@@ -23,7 +23,7 @@ typedef struct {
   uint32_t (* uart_cache_usage)(void);
 } uart_dev_t;
 
-status_t periph_get_uart_drv(const uart_dev_t *drv, const char *name);
+//status_t periph_get_uart_drv(uart_dev_t *drv, const char *name);
 
 status_t periph_disabled(const char *tag);
 
--- ctrl/driver/periph/src/periph.c
+++ ctrl/driver/periph/src/periph.c
@@ -8,74 +8,63 @@
 #include "periph.h"
 #include <string.h>
 
-#include "usart1.h"
-#include "usart2.h"
-#include "uart4.h"
-#include "usart6.h"
-
 #include "log.h"
 
-static const uart_dev_t usart1_dev = {
-  "ttyS1",
-  usart1_init,
-  usart1_deinit,
-  usart1_rx_bytes,
-  usart1_tx_bytes,
-  usart1_tx_bytes_it,
-  usart1_tx_bytes_dma,
-  usart1_cache_usage,
-};
+//extern const uart_dev_t usart1_dev;
+//extern const uart_dev_t usart2_dev;
+//extern const uart_dev_t uart4_dev;
+//extern const uart_dev_t usart6_dev;
 
-static const uart_dev_t usart2_dev = {
-  "ttyS2",
-  usart2_init,
-  usart2_deinit,
-  usart2_rx_bytes,
-  usart2_tx_bytes,
-  usart2_tx_bytes_it,
-  usart2_tx_bytes_dma,
-  usart2_cache_usage,
-};
+//typedef struct {
+//  const char *name;
+//  const uart_dev_t *dev;
+//} uart_dev_list_t;
+//
+//static const uart_dev_list_t uart_dev_list[] = {
+//  {
+//    "ttyS1",
+//	&usart1_dev,
+//  },
+//  {
+//    "ttyS2",
+//	&usart2_dev,
+//  },
+//  {
+//    "ttyS4",
+//	&uart4_dev,
+//  },
+//  {
+//    "ttyS6",
+//	&usart6_dev,
+//  },
+//};
 
-static const uart_dev_t uart4_dev = {
-  "ttyS4",
-  uart4_init,
-  uart4_deinit,
-  uart4_rx_bytes,
-  uart4_tx_bytes,
-  uart4_tx_bytes_it,
-  uart4_tx_bytes_dma,
-  uart4_cache_usage,
-};
-
-static const uart_dev_t usart6_dev = {
-  "ttyS6",
-  usart6_init,
-  usart6_deinit,
-  usart6_rx_bytes,
-  usart6_tx_bytes,
-  usart6_tx_bytes_it,
-  usart6_tx_bytes_dma,
-  usart6_cache_usage,
-};
-
-static const uart_dev_t *uart_dev_list[] = {
-  &usart1_dev,
-  &usart2_dev,
-  &uart4_dev,
-  &usart6_dev,
-};
-
-status_t periph_get_uart_drv(const uart_dev_t *drv, const char *name)
-{
-  for(uint32_t i = 0; i < (sizeof(uart_dev_list) / sizeof(uart_dev_t *)); i ++) {
-    if(strcmp(name, uart_dev_list[i]->uart_name) == 0) {
-      drv = uart_dev_list[i];
-      return status_ok;
-    }
-  }
-  return status_error;
-}
+//status_t periph_get_uart_drv(uart_dev_t *drv, const char *name)
+//{
+////  for(uint32_t i = 0; i < (sizeof(uart_dev_list) / sizeof(uart_dev_list_t)); i ++) {
+////    if(strcmp(name, uart_dev_list[i].name) == 0) {
+////      drv = uart_dev_list[i].dev;
+////      return status_ok;
+////    }
+////  }
+//  if(strcmp(name, "ttyS1") == 0) {
+//    drv = &usart1_dev;
+//    return status_ok;
+//  }
+//  if(strcmp(name, "ttyS2") == 0) {
+//    drv = &usart2_dev;
+//    return status_ok;
+//  }
+//  if(strcmp(name, "ttyS4") == 0) {
+//    drv = &uart4_dev;
+//    return status_ok;
+//  }
+//  if(strcmp(name, "ttyS6") == 0) {
+//    drv = &usart6_dev;
+//    return status_ok;
+//  }
+//  return status_error;
+//}
 
 status_t periph_disabled(const char *tag)
 {
