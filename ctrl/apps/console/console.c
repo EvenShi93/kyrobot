985f7a7aa57504afd82310d1f71defb40a29bc4e
kyChu <hello.kychu@gmail.com>
2020-05-25 14:55:00 +0800

    initial commit for console based on shell module

---
+++ ctrl/apps/console/console.c
@@ -0,0 +1,140 @@
+/*
+ * console.c
+ *
+ *  Created on: May 14, 2020
+ *      Author: kychu
+ */
+
+#include "console.h"
+
+#if FREERTOS_ENABLED
+#include "cmsis_os.h"
+
+#include <ctype.h>
+#include <string.h>
+
+#include "log.h"
+
+#define DEFAULT_CONSOLE_PROMPT              "kychu>"
+
+enum KEY_ACTION{
+	KEY_NULL = 0,	    /* NULL */
+	CTRL_A = 1,         /* Ctrl+a */
+	CTRL_B = 2,         /* Ctrl-b */
+	CTRL_C = 3,         /* Ctrl-c */
+	CTRL_D = 4,         /* Ctrl-d */
+	CTRL_E = 5,         /* Ctrl-e */
+	CTRL_F = 6,         /* Ctrl-f */
+	CTRL_H = 8,         /* Ctrl-h */
+	TAB = 9,            /* Tab */
+	CTRL_K = 11,        /* Ctrl+k */
+	CTRL_L = 12,        /* Ctrl+l */
+	ENTER = 10,         /* Enter */
+	CTRL_N = 14,        /* Ctrl-n */
+	CTRL_P = 16,        /* Ctrl-p */
+	CTRL_T = 20,        /* Ctrl-t */
+	CTRL_U = 21,        /* Ctrl+u */
+	CTRL_W = 23,        /* Ctrl+w */
+	ESC = 27,           /* Escape */
+	BACKSPACE =  127    /* Backspace */
+};
+
+typedef struct {
+  uint8_t ln_cache[CONSOLE_LINE_CACHE_SIZE];
+} console_cache_t;
+
+static const char *TAG = "CONSOLE";
+
+static char **argv;
+static console_cache_t *console_cache;
+
+static void console_task(void const *argument);
+static void start_action(console_cfg_t *console);
+static int console_proc(console_cfg_t *console);
+
+status_t console_start(console_cfg_t *console)
+{
+  if(console == NULL) return status_error;
+  if(console->prompt == NULL) console->prompt = DEFAULT_CONSOLE_PROMPT;
+  console_cache = kmm_alloc(sizeof(console_cache_t));
+  if(console_cache == NULL) return status_nomem;
+  memset(console_cache, 0, sizeof(console_cache_t));
+  argv = (char **)kmm_alloc(CONSOLE_MAX_CMDLINE_ARGS * sizeof(char *));
+  if(argv == NULL) return status_nomem;
+  memset(argv, 0, CONSOLE_MAX_CMDLINE_ARGS * sizeof(char *));
+  osThreadDef(CONSOLE, console_task, osPriorityNormal, 0, 256);
+  if(osThreadCreate(osThread(CONSOLE), console) == NULL) return status_error;
+  return status_ok;
+}
+
+static void console_task(void const *argument)
+{
+  int argc, cmd_ret;
+  console_cfg_t *console = (console_cfg_t *)argument;
+  start_action(console);
+  console->console_tx((uint8_t *)"\r\nkyChu Shell\r\n", 15);
+  for(;;) {
+    // start new command line
+    console->console_tx((uint8_t *)console->prompt, strlen(console->prompt));
+    if(console_proc(console) > 0) {
+      argc = shell_split_argv((char *)console_cache->ln_cache, argv, CONSOLE_MAX_CMDLINE_ARGS);
+      if(shell_cmd_run(argc, argv, &cmd_ret) == status_not_found) {
+        ky_err(TAG, "\'%s\' command NOT found", argv[0]);
+      } else {
+        ky_info(TAG, "exit with (%d)", cmd_ret);
+      }
+    }
+  }
+}
+
+static void start_action(console_cfg_t *console)
+{
+  // press Enter 3 times.
+  uint8_t c, count = 0;
+  do {
+    if(console->console_rx(&c, 1) == 0) {
+      delay(10);
+    } else {
+      if(c == '\r') {
+        count ++;
+      } else {
+        count = 0;
+      }
+    }
+  } while(count < 3);
+}
+
+static int console_proc(console_cfg_t *console)
+{
+  uint8_t c;
+  uint32_t count = 0;
+  while(1) {
+    if(console->console_rx(&c, 1) == 0) {
+      delay(10);
+    } else {
+      if (c == '\r') {
+        console->console_tx((uint8_t *)"\r\n", 2);
+        break;
+      } else if (c == 0x1b) {//(c >= 0x1c && c <= 0x1f){
+        continue; /* consume arrow keys */
+      } else if (c == BACKSPACE || c == 0x8) {
+        if (count > 0) {
+          console_cache->ln_cache[count - 1] = 0;
+          count --;
+          console->console_tx((uint8_t *)"\x08 ", 2); /* Windows CMD: erase symbol under cursor */
+        } else continue;
+      } else {
+        console_cache->ln_cache[count ++] = c;
+        if(count == CONSOLE_LINE_CACHE_SIZE - 1) {
+          console->console_tx((uint8_t *)"\r\n", 2);
+          break;
+        }
+      }
+      console->console_tx((uint8_t *)&c, 1); /* echo */
+    }
+  }
+  console_cache->ln_cache[count] = 0;
+  return count;
+}
+
+#endif /* FREERTOS_ENABLED */
---
+++ ctrl/apps/console/console.h
@@ -0,0 +1,24 @@
+/*
+ * console.h
+ *
+ *  Created on: May 14, 2020
+ *      Author: kychu
+ */
+
+#ifndef CONSOLE_CONSOLE_H_
+#define CONSOLE_CONSOLE_H_
+
+#include "shell.h"
+
+#define CONSOLE_LINE_CACHE_SIZE        128
+#define CONSOLE_MAX_CMDLINE_ARGS       8
+
+typedef struct {
+  const char *prompt;
+  uint32_t (* console_rx)(uint8_t *p, uint32_t l);
+  status_t (* console_tx)(uint8_t *p, uint32_t l);
+} console_cfg_t;
+
+status_t console_start(console_cfg_t *console);
+
+#endif /* CONSOLE_CONSOLE_H_ */
